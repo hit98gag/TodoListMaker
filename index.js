306253196f452e15733f4c225aac157ea29d3e1f
Hitesh Tiwari <72449162+hit98gag@users.noreply.github.com>
2022-11-12 00:12:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var input = document.getElementsByClassName("in")[0];
+
+var add = document.getElementsByClassName("add")[0];
+
+var tasks = document.getElementsByClassName("tasks")[0];
+
+function addToList(){
+
+    var item = document.createElement("DIV");
+    
+    item.innerHTML = '<i class="fa-solid fa-check-double"></i> '+input.value;
+    item.classList.add("listitem");
+    tasks.appendChild(item);
+    input.value ="";
+
+    item.addEventListener("click", function()
+    {
+        this.remove();
+    })
+}
+
+add.addEventListener("click", addToList)
